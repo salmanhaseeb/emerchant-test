9596662879d573312b9b39fbf2c23047f924d1e2
salmanhaseeb <salmanhaseeb@gmail.com>
2022-03-13 15:48:15 +0500

    initializing admin

---
+++ app/controllers/admins/merchants_controller.rb
@@ -0,0 +1,17 @@
+class Admins::MerchantsController < ApplicationController
+  before_action :authenticate_admin!
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
---
+++ spec/requests/admins/merchants_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Admins::Merchants", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/admins/merchants/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/admins/merchants/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/admins/merchants/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /update" do
+    it "returns http success" do
+      get "/admins/merchants/update"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/admins/merchants/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
